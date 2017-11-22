2e38e826c0bd7943677504de24f6303674036a35
joepike <joseph.l.pike@gmail.com>
2017-11-22 16:51:38 +0000

    single responsibility implemented

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -4,17 +4,38 @@ class DockingStation
   attr_reader :bikes
 
   def initialize()
-    @bikes = []
+    @docked = Docked.new
   end
 
   def dock_bike(bike)
-    @bikes << bike
+    raise "Docking station full" if full?
+    @docked.bikes << bike
   end
 
   def release_bike
     # Bike.new
-    raise "No bikes available" if @bikes.empty?
-    #fail "No bikes available" unless @bikes
-    @bikes.pop
+    raise "No bikes available" if empty?
+    #fail "No bikes available" unless @docked.bikes
+    @docked.bikes.pop
   end
+
+  private
+
+  def full?
+    @docked.bikes.count == 20 ? true : false
+  end
+
+  def empty?
+    @docked.bikes.empty?
+  end
+
+end
+
+class Docked
+  attr_reader :bikes
+
+  def initialize()
+    @bikes = []
+  end
+
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -7,6 +7,7 @@ describe DockingStation do
     it 'returns an error when no bikes available' do
       expect { subject.release_bike }.to raise_error "No bikes available"
     end
+
     it 'releases bikes' do
       subject.dock_bike(Bike.new)
       bike = subject.release_bike
@@ -16,8 +17,14 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
-  it 'docks bikes' do
-    bike = Bike.new
-    expect(subject.dock_bike(bike)).to include bike
+  describe '#dock_bike' do
+    it 'docks bikes' do
+      bike = Bike.new
+      expect(subject.dock_bike(bike)).to include bike
+    end
+    it 'returns an error when docking station full' do
+      20.times { subject.dock_bike(Bike.new) }
+      expect { subject.dock_bike(Bike.new) }.to raise_error "Docking station full"
+    end
   end
 end
