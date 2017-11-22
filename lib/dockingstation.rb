30a8c540341ddb142bb96decbfaf72eaf357067b
kkumshayev <kumshkesh@gmail.com>
2017-11-22 12:38:34 +0000

    passes tests for docking bikes, bikes array implemented with instance variable & attribute reader

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,7 +1,18 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bikes
+
+  def initialize()
+    @bikes = []
+  end
+
+  def dock_bike(bike)
+    @bikes << bike
+  end
+
   def release_bike
     Bike.new
+    # @bikes.empty? ? "There are no bikes" : @bikes.pop
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,6 +5,13 @@ describe DockingStation do
 
   it 'releases bikes' do
     bike = subject.release_bike
-    expect(bike).to be_working
+    expect(bike.working?).to eq true
+  end
+
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+  it 'docks bikes' do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to include bike
   end
 end
